beb5d14b2be2e1524ca15ffa3fa6aa3fe303c737
MirkOrlando <mirko@orlando-m.it>
2022-05-24 16:05:02 +0200

    milestone 2 get data from an api call

--- milestone_1/db.php
+++ milestone_1/db.php
@@ -4,3 +4,7 @@ $api_call = json_decode(file_get_contents("https://flynn.boolean.careers/exercis
 //var_dump($api_call);
 $albums = $api_call['response'];
 //var_dump($albums);
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+echo json_encode($albums);
--- milestone_2/src/App.vue
+++ milestone_2/src/App.vue
@@ -1,19 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      albums: null,
+    };
+  },
+  mounted() {
+    axios
+      .get("http://localhost/PHP/php-ajax-dischi/milestone_1/db.php")
+      .then((response) => {
+        //console.log(response);
+        this.albums = response.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
 </script>
 
 <style lang="scss">
