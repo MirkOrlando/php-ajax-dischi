c3b6e0bd7d8e155727124d42f5826889dbf65f17
MirkOrlando <mirko@orlando-m.it>
2022-05-24 16:38:46 +0200

    milestone 2 create main

--- milestone_2/src/App.vue
+++ milestone_2/src/App.vue
@@ -5,7 +5,34 @@
         <img class="img-fluid" src="@/assets/img/spotify-logo.png" alt="logo" />
       </div>
     </header>
-    <main>SITE MAIN HERE</main>
+    <main>
+      <div class="container py-4">
+        <div
+          class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-3"
+        >
+          <div class="col" v-for="(album, index) in albums" :key="index">
+            <div class="card h-100 p-3 text-center">
+              <div class="card_poster">
+                <img
+                  class="card-img-top img-fluid h-100"
+                  :src="album.poster"
+                  :alt="album.title"
+                />
+              </div>
+              <div class="card-body p-0 pt-4">
+                <h4 class="card-title text-uppercase fw-bold">
+                  {{ album.title }}
+                </h4>
+                <div class="card-text">
+                  <div class="author">{{ album.author }}</div>
+                  <div class="year">{{ album.year }}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
     <footer>SITE FOOTER HERE</footer>
   </div>
 </template>
