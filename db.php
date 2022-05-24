4c8b092db1e859cb8c0f0a93b588f63e6d51e01b
MirkOrlando <mirko@orlando-m.it>
2022-05-24 15:04:45 +0200

    create header

--- db.php
+++ db.php
@@ -3,4 +3,4 @@
 $api_call = json_decode(file_get_contents("https://flynn.boolean.careers/exercises/api/array/music"), true);
 //var_dump($api_call);
 $albums = $api_call['response'];
-//var_dump($api_call['response']);
+//var_dump($albums);
--- partials/head.php
+++ partials/head.php
@@ -9,5 +9,7 @@
 
     <!-- Bootstrap CSS v5.1.3 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <style>
+        <?php include __DIR__ . '/../assets/css/style.css' ?>
+    </style>
 </head>
