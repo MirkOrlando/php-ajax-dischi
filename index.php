008bac2e368db0310d55e88c2e176bc49b11477d
MirkOrlando <mirko@orlando-m.it>
2022-05-24 15:23:28 +0200

    create footer.php

--- index.php
+++ index.php
@@ -3,13 +3,4 @@ include __DIR__ . '/db.php';
 include __DIR__ . '/partials/head.php';
 include __DIR__ . '/partials/header.php';
 include __DIR__ . '/partials/main.php';
-
-?>
-
-
-<!-- Bootstrap JavaScript Libraries -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-
-</html>
+include __DIR__ . '/partials/footer.php';
